7b10822505b55858bacbaa7f618d6afc9c67b043
Eunjin3395 <jennyeunjin@khu.ac.kr>
2025-11-23 21:15:24 +0900

    참여자 추가

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ const USERNAME_TO__DISCORD_ID = {
   최문형: "invite_me_41",
   김호준: "gimhojun0668",
   이제희: "incredible_dragon_84712",
+  최선하: "seon3831",
 };
 
 const DAYOFF_CHANNEL_ID = process.env.DAYOFF_CHANNEL_ID;
